b51f26832bd6f8472095ebe45cdba5a7c5aca928
casianZ <nicuzbona@gmail.com>
2019-08-08 17:23:27 +0300

    Setting up heroku env variables for firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
 };
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig); 
 
 
 const database = firebase.database();
